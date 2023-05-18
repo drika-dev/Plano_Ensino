d8534f9b0d83b43f371cce7d6135f5cabcfbcf2e
Drikadev <126597972+drika-dev@users.noreply.github.com>
2023-05-18 14:15:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./App.css";
 
 function App() {
+
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+
+  const database = [
+    {
+      username: "adriane",
+      password: "123456"
+    },
+    {
+      username: "davi",
+      password: "123456"
+    }
+  ];
+
+  const errors = {
+    userName: "Usuário Inválido",
+    passWord: "Senha inválida"
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const userData = database.find((user) => user.username === userName);
+
+    if (userData) {
+      if (userData.password !== passWord) {
+        setErrorMessages({ name: "passWord", message: errors.passWord });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+
+      setErrorMessages({ name: "userName", message: errors.userName });
+    }
+  };
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  const renderForm = (
+    <div class="contact-us">
+      <form onSubmit={handleSubmit}>
+        <div class="input-container">
+          <input
+            type="text"
+            name="userName"
+            placeholder="Nome"
+            required
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          {renderErrorMessage("userName")}
+        </div>
+        <div class="input-container">
+          <input
+            type="password"
+            name="passWord"
+            placeholder="Senha"
+            required
+            value={passWord}
+            onChange={(e) => setPassWord(e.target.value)}
+          />
+          {renderErrorMessage("passWord")}
+        </div>
+        <div class="button-container">
+          <button type="submit">Enviar Dados</button>
+        </div>
+      </form>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="app">
+      <div class="login-form">
+        {isSubmitted ? <div>Usuário Logado</div> : renderForm}
+      </div>
     </div>
   );
 }
